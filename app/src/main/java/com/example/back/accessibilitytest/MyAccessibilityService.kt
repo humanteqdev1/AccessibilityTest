80fb330f3c82860c51e57e2242a3c58611658209
humanteqdev1 <kirill.savelyev@humanteq.io>
2018-04-18 19:23:47 +0300

    --

--- app/src/main/java/com/example/back/accessibilitytest/MyAccessibilityService.kt
+++ app/src/main/java/com/example/back/accessibilitytest/MyAccessibilityService.kt
@@ -7,8 +7,7 @@ import android.view.accessibility.AccessibilityNodeInfo
 import android.accessibilityservice.AccessibilityServiceInfo
 import android.os.Build
 import android.os.SystemClock
-import android.view.accessibility.AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED
-import android.view.accessibility.AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED
+import android.view.accessibility.AccessibilityEvent.*
 import android.view.accessibility.AccessibilityRecord
 
 
@@ -52,39 +51,44 @@ class MyAccessibilityService : AccessibilityService() {
              * Not working for: Firefox;
              * Works(?) for: Chrome;
              */
-            Log.e("234", "scroll: ${getScrollPosition(event)}}")
+            if(event.eventType == TYPE_VIEW_SCROLLED && event.className == WEBVIEW)
+                Log.e("scrollY", "scroll: ${getScrollPosition(event)}")
 
 
-            if(urlArray.isNotEmpty()) {
-                Log.e("map", "${urlArray.size}=${urlArray.toList()}")
-            }
+//            if(urlArray.isNotEmpty()) {
+//                Log.e("map", "${urlArray.size}=${urlArray.toList()}")
+//            }
         }
     }
 
     private fun getScrollPosition(event: AccessibilityEvent): Float {
-        val record = AccessibilityRecord.obtain(event)
-        val itemCount = event.itemCount
-        val fromIndex = event.fromIndex
+            val itemCount = event.itemCount
+            val fromIndex = event.fromIndex
 
-        // First, attempt to use (fromIndex / itemCount).
-        if (fromIndex >= 0 && itemCount > 0) {
-            return fromIndex / itemCount.toFloat()
-        }
+            // First, attempt to use (fromIndex / itemCount).
+            if (fromIndex >= 0 && itemCount > 0) {
+                return fromIndex / itemCount.toFloat()
+            }
 
-        val scrollY = record.scrollY
-        val maxScrollY = record.maxScrollY
+            val scrollY = event.scrollY
+            val maxScrollY = event.maxScrollY
 
-        // Next, attempt to use (scrollY / maxScrollY). This will fail if the
-        // getMaxScrollX() method is not available.
-        if (scrollY >= 0 && maxScrollY > 0) {
-            return scrollY / maxScrollY.toFloat()
-        }
+            if (scrollY > -1) {
 
-        // Finally, attempt to use (scrollY / itemCount).
-        // TODO(alanv): Hack from previous versions -- is it still needed?
-        return if (scrollY >= 0 && itemCount > 0 && scrollY <= itemCount) {
-            scrollY / itemCount.toFloat()
-        } else 0.5f
+                // Next, attempt to use (scrollY / maxScrollY). This will fail if the
+                // getMaxScrollX() method is not available.
+                if (scrollY >= 0 && maxScrollY > 0) {
+                    return scrollY / maxScrollY.toFloat()
+                }
+
+                // Finally, attempt to use (scrollY / itemCount).
+                // TODO(alanv): Hack from previous versions -- is it still needed?
+                return if (scrollY >= 0 && itemCount > 0 && scrollY <= itemCount) {
+                    scrollY / itemCount.toFloat()
+                } else -1f
+            }
+
+        return -1f
     }
 
     private fun getTextFromEvents(event: AccessibilityEvent,
@@ -107,6 +111,7 @@ class MyAccessibilityService : AccessibilityService() {
     }
 
     private val URL = "URL"
+    private val WEBVIEW = "android.webkit.WebView"
     private val TEXTVIEW = "android.widget.TextView"
     private val EDITTEXT = "android.widget.EditText"
     private val CHROME = "com.android.chrome"
@@ -116,10 +121,11 @@ class MyAccessibilityService : AccessibilityService() {
     private val CHROME_URL_BAR_ID = "$CHROME:id/url_bar"
     private val MOZILLA_FIREFOX_BROWSER_TOOLBAR_ID = "org.mozilla.firefox:id/browser_toolbar"
 
-    private val urlArray = arrayListOf<Pair<String?, Long?>>()
+    private val urlArray = arrayListOf<Triple<String?, Long?, Float>>()
 
     private var prevUrl: String? = null
     private var prevStartTime: Long = 0L
+    private var prevScrollY: Long = 0L
     private fun collect(info: AccessibilityNodeInfo?,
                         packageNameList: List<String>,
                         collectTypeList: List<String>,
@@ -174,8 +180,8 @@ class MyAccessibilityService : AccessibilityService() {
         val url = ch.text.toString()
         if(prevUrl != url) {
             if(prevUrl != null) {
-                urlArray.add(Pair(prevUrl.toString(),
-                        SystemClock.elapsedRealtime() - prevStartTime))
+                urlArray.add(Triple(prevUrl.toString(),
+                        SystemClock.elapsedRealtime() - prevStartTime, 0f))
             }
             prevUrl = url
             prevStartTime = event.eventTime
